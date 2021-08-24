09465334d7010efc118f551b10a28871d162038f
2101class <1742731732@.com>
2021-08-24 21:10:09 +0800

    小明2.0

--- src/main/java/cn/baizhi/A.java
+++ src/main/java/cn/baizhi/A.java
@@ -1,8 +1,12 @@
 package cn.baizhi;
-
+//小明
 public class A {
     public static void main(String[] args) {
         int a=10;
         System.out.println(a);
+
+
+        int b=20;
+        System.out.println(b);
     }
 }
