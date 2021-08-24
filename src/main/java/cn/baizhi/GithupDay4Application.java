268a310bbdef6af5a4c182b260ae0382fd5753db
2101class <1742731732@.com>
2021-08-24 20:09:46 +0800

    小明1.0

---
+++ src/main/java/cn/baizhi/GithupDay4Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithupDay4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithupDay4Application.class, args);
+    }
+
+}
